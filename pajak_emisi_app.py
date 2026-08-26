5412f76008520f00b293d9872a8ea8ef2a005570
seagatecodeset <108435586+seagatecodeset@users.noreply.github.com>
2026-08-26 14:43:54 +0700

    Update pajak_emisi_app.py

--- pajak_emisi_app.py
+++ pajak_emisi_app.py
@@ -972,7 +972,7 @@ if st.button("🔍 Simulasikan PKB Emisi"):
         st.write(f"**Koefisien Emisi (KE):** {ke:.6f}")
 
     # Tampilkan status dengan warna sesuai hasil emisi
-    if rasio_emisi_awal <= 1:
+    if rasio_emisi <= 1:
         st.success(status_emisi)
     else:
         st.error(status_emisi)
